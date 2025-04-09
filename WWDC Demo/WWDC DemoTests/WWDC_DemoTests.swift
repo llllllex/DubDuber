e1ce69e4cf040321e142fa563a32d8657490e572
Lex <kfc56@live.cn>
2025-04-09 23:56:55 +0800

    init: initialize project

---
+++ WWDC Demo/WWDC DemoTests/WWDC_DemoTests.swift
@@ -0,0 +1,16 @@
+//
+//  WWDC_DemoTests.swift
+//  WWDC DemoTests
+//
+//  Created by Lex on 4/9/25.
+//
+
+import Testing
+
+struct WWDC_DemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
